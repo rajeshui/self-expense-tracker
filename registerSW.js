a8653e1188919e675a8f2d01c50bdbe79531417f
rajesh <raji.subam@gmail.com>
2025-12-25 22:34:34 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/self-expense-tracker/sw.js', { scope: '/self-expense-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/self-expense-tracker/sw.js', { scope: '/' })})}
